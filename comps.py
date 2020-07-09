9f62c2076034f8d20b66b2090e72502b9f128b8d
seanaleid <seanaleid@gmail.com>
2020-07-09 14:01:15 -0400

    Example 5 notes

---
+++ comps.py
@@ -0,0 +1,23 @@
+people = ["Abe", "Bill", "Charles", "Dolly", "David", "Evelyn", "Frank", "Gunther"]
+
+# comp for names that start with D
+# a = [<map expression> for person in people <filter expression>]
+# a = [name for name in people if name.startswith("D")]
+# print(a)
+# b = []
+
+# for name in people:
+#     if name.startswith("D"):
+#         b.append(name)
+# print(b)
+
+
+# comp for names that end in Y
+
+# comp for names that start with B through D
+# d = [name for name in people if name.startswith("B")]
+# print(d)
+
+# comp for names in uppercase
+# c = [name.upper() for name in people]
+# print(c)
---
+++ product_reader.py
@@ -0,0 +1,8 @@
+class Product:
+    def __int__(self, name, price)
+        self.name = name
+        self.price = price
+
+products = []
+
+def productreader(products=[]):
