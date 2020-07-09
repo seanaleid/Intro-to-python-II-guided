997b5578b87a939e51fffdb5dc1afa5fe72fe5e2
seanaleid <seanaleid@gmail.com>
2020-07-09 12:24:32 -0400

    Example 1 notes

---
+++ scratch.py
@@ -0,0 +1,13 @@
+
+
+### example 1
+# class Bicycle:
+#     def exclaim(self):
+#         print("I'm a bicycle!")
+
+# class Specialized(Bicycle):
+#     def jump(self):
+#         print("I'm jumping!")
+
+# a_bicycle = Bicycle()
+# a_specialized = Specialized()
