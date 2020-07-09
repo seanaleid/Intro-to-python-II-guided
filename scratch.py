0d6b89d5944feb997948dedad43f0b03e976bd56
seanaleid <seanaleid@gmail.com>
2020-07-09 12:44:43 -0400

    Example 2 notes

--- scratch.py
+++ scratch.py
@@ -1,4 +1,13 @@
+### example 2
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
 
+# class Graduate(Student):
+#     def __init__(self, name, graduation_date):
+#         # looks up to the Student parent class and Graduate child class inherits it
+#         super().__init__(name)
+#         self.graduation_date = graduation_date
 
 ### example 1
 # class Bicycle:
